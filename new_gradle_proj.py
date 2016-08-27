c7d4262c12bba206d953e99f56d04bee2fb2ee92
SpencerA <spnrpa@gmail.com>
2016-08-27 16:00:24 -0700

    automate new java project creation

---
+++ new_gradle_proj.py
@@ -0,0 +1,69 @@
+"""create a empty gradle project with default layout"""
+
+
+import os
+import argparse
+import shutil
+import sys
+
+class chdir:
+  def __init__(self, path):
+    self.newpath=os.path.expanduser(path)
+
+  def __enter__(self):
+    self.oldpath=os.getcwd()
+    os.chdir(self.newpath)
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    os.chdir(self.oldpath)
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--d", help="project name", type=str)
+  parser.add_argument("--p", help="path to project home", type=str)
+  return parser.parse_args()
+
+def build_root(root_name):  # assumes cur directory is the project root
+  os.mkdir(root_name)
+  return os.path.abspath(os.path.join(".", root_name))
+
+def build_src(proj_path):
+  os.makedirs(os.path.join(proj_path, "src", "main", "java"))
+  os.makedirs(os.path.join(proj_path, "src", "test", "java"))
+
+def copy_gradle_files(proj_path, gsrc):
+  gfiles = os.listdir(gsrc)
+  for f in gfiles:
+    path = os.path.join(gsrc,f)
+    if os.path.isfile(path):
+      shutil.copy(path, proj_path)
+    else:
+      shutil.copytree(path, os.path.join(proj_path, f))
+
+if __name__ == "__main__":
+  args = parse_args()
+  if os.path.exists(args.p):
+    with chdir(args.p) as p:
+      proj_path = build_root(args.d)
+      try:
+        build_src(proj_path)
+        copy_gradle_files(proj_path, os.path.join(p.oldpath, "newGradleProjectFiles"))
+      except Exception as e:
+        print e
+        print("Exception caught... Undoing project creation")
+        shutil.rmtree(proj_path)
+        sys.exit(1)
+  else:
+    raise ValueError("directory does not exist: {}.".format(args.p))
+#  os.makedirs("src/main/java")
+#  os.makedirs("src/test/java")
+  # gradle.properties
+  # gradlew
+  # gradle.bat
+  # build.gradle
+  # settings.gradle
+  #.gitignore
+  # src/main/java
+  # src/test/java
+
